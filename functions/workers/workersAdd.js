0d7aa766034e79406f5658abfc21877485be0be5
Sergey Pechinov <sen1c163rus@gmail.com>
2019-12-03 22:24:22 +0400

    workers update

--- functions/workers/workersAdd.js
+++ functions/workers/workersAdd.js
@@ -9,6 +9,8 @@ module.exports = (req, res) => {
 			workerPosition = req.body.position,
 			workerCabinet = req.body.cabinet;
 
+	console.log(workerSurname.length)
+
 	if (workerSurname.length > 1 &&
 			workerName.length > 1 &&
 			workerPosition.length > 0 &&
@@ -21,13 +23,30 @@ module.exports = (req, res) => {
 
 		clientDB.query(queryAddWorker, (err, result) => {
 			if (err) {
-				console.log(err)
+				// console.log(err)
 			} else {
-				console.log('Работник добавлен!');
+				// console.log('Работник добавлен!');
 				res.status(200).json({
 					success: true,
 				});
 			}
+		});
+	} else {
+		let errorsStatus = {
+			surname: '',
+			name: '',
+			position: '',
+			cabinet: '',
+		};
+
+		workerSurname.length < 2 ? errorsStatus.surname = 'Минимум 2 символа!' : null;
+		workerName.length < 2 ? errorsStatus.name = 'Минимум 2 символа!' : null;
+		workerPosition.length < 1 ? errorsStatus.position = 'Минимум 1 символ!' : null;
+		workerCabinet.length < 1 ? errorsStatus.cabinet = 'Минимум 1 символ!' : null;
+
+		res.status(400).json({
+			success: false,
+			message: errorsStatus,
 		});
 	}
 };
